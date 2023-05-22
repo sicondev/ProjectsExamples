37271b5dcf41d0f506b2b522fe6bc1592e4486b8
sicondev <65614289+sicondev@users.noreply.github.com>
2023-05-22 12:42:51 +0100

    Update POPMethods.cs

--- POPMethods.cs
+++ POPMethods.cs
@@ -1,6 +1,8 @@
 using Sicon.Sage200.Projects.Objects;
 using Sicon.Sage200.Projects.Objects.Coordinators;
 using Sicon.Sage200.Projects.Objects.Factory;
+using Sicon.Sage200.Projects.Objects.Infrastructure.ProjectTransactions.Coordinators;
+using Sicon.Sage200.Projects.Objects.Instruments;
 using System;
 
 namespace ProjectsExamples
@@ -72,6 +74,98 @@ namespace ProjectsExamples
             {
                 //Update project analysis on the order header
                 POPPostingCoordinator.UpdateOrderHeaderProjectAnalysis(POPOrderReturnID, ProjectNumber, ProjectHeaderID, PhaseID, StageID, ActivityID, 0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Create Project transaction for POP invoice and reconcile
+        /// </summary>
+        /// <param name="oPOPOrderReturnLine"></param>
+        /// <param name="oPLEntry"></param>
+        /// <param name="oProject"></param>
+        /// <param name="oProjectHeader"></param>
+        /// <param name="PhaseID"></param>
+        /// <param name="StageID"></param>
+        /// <param name="ActivityID"></param>
+        /// <param name="oNLCode"></param>
+        /// <param name="ThisNarrative"></param>
+        /// <param name="LineTotalValue"></param>
+        public void ReconcilePOPInvoiceToProjectAndNominal(Sage.Accounting.POP.POPOrderReturnLine oPOPOrderReturnLine, Sage.Accounting.PurchaseLedger.PostedPurchaseAccountEntry oPLEntry, SiJcJob oProject, SiJcChd oProjectHeader, long PhaseID, long StageID, long ActivityID,
+            Sage.Accounting.NominalLedger.NominalCode oNLCode, string ThisNarrative, decimal LineTotalValue)
+        {
+            try
+            {
+                string TranType = "POPINV";
+                if (oPLEntry.EntryType == Sage.Accounting.TradingAccountEntryTypeEnum.TradingAccountEntryTypeCreditNote)
+                {
+                    TranType = "POPCredit";
+                }
+                ProjectAnalysisItem oProjectAnalysisItem = ProjectAnalysisItemFactory.Factory.CreateNew();
+
+                //Populate fields for POP document type transaction
+                oProjectAnalysisItem.Generate(oProject, oProjectHeader, oPLEntry.InstrumentDate, oNLCode, ThisNarrative, LineTotalValue, "", TranType, oPLEntry.URN, oPLEntry.PLPostedSupplierTran, true, false, false, PhaseID, StageID, ActivityID);
+
+                //Fill in some extra fields
+                oProjectAnalysisItem.ThisQuantity = 1;
+                oProjectAnalysisItem.Quantity = 1;
+
+                oProjectAnalysisItem.Supplier = oPLEntry.Supplier;
+                oProjectAnalysisItem.POPOrderReturnLine = oPOPOrderReturnLine;
+                oProjectAnalysisItem.POPOrderReturn = oPOPOrderReturnLine.POPOrderReturn;
+                oProjectAnalysisItem.PLEntry = oPLEntry;
+
+                //Post transaction creating SIJCTRN and reconciling to nominal
+                oProjectAnalysisItem.Post();
+
+                //Update fields after post (Optional)
+                oProjectAnalysisItem.SiJcTrn.InvoiceCreditNumber = oPLEntry.InstrumentNo;
+                oProjectAnalysisItem.SiJcTrn.InvoiceCreditDate = oPLEntry.InstrumentDate;
+                oProjectAnalysisItem.SiJcTrn.Update();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        public void PostPOPInvoiceAndCreateProjectTransaction()
+        {
+            try
+            {
+                PostPOPInvoiceCoordinator oCoordinator = new PostPOPInvoiceCoordinator()
+                {
+                    SupplierReference = "ABC001",//Supplier Reference
+                    InvoiceCreditDate = DateTime.Today,//Invoice date
+                    InvoiceCreditNo = "INV008",//Invoice number
+                    //OPTIONAL: add invoice value (*Variance will be added as an extra nominal line)
+                    InvCredGoodsValue = 120,
+                    InvCredTaxValue = 25,
+                };
+
+                //Loop Through Orders and lines available to invoice
+
+                POPInvoiceItem oInstrument = null;
+                oInstrument = new POPInvoiceItem()
+                {
+                    OrderDocumentNo = "0000000066"//POP Order Number
+                };
+
+                
+                oInstrument.POPLineIDs.Add(Convert.ToInt64(515123)); //POP order line ID
+                oCoordinator.POPInvoiceItems.Add(oInstrument);
+
+
+                //Post
+                long URN = oCoordinator.PostInvoice();
+
             }
             catch (Exception)
             {
